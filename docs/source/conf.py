d0e3952130c80e18261a8cfc7e725825b33415c6
aouwalitshikkha <aouwalitshikkha@gmail.com>
2022-04-04 00:37:17 +0600

    Added Usage Section

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Dghippo'
+copyright = '2021, Dghippo'
+author = 'Dghippo'
 
 release = '0.1'
 version = '0.1.0'
